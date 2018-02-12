b7e97e831b5409ca25c11dcf7bce87335814b9f2
nikita-vanyasin <nikita.vanyasin@gmail.com>
2018-02-12 21:00:28 +0300

    replaced i3-nagbar with zenity dialogs

--- ansible/roles/i3wm/templates/check-battery-level.sh
+++ ansible/roles/i3wm/templates/check-battery-level.sh
@@ -2,5 +2,5 @@
 
 BATTINFO=`acpi -b`
 if [[ `echo $BATTINFO | grep Discharging` && `echo $BATTINFO | cut -f 5 -d " "` < 00:15:00 ]] ; then
-    DISPLAY=:0.0 i3-nagbar "Low battery! $BATTINFO"
+    DISPLAY=:0.0 zenity --error --text "Low battery! $BATTINFO"
 fi
