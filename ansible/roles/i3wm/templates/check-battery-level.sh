90a1725a2f777a6dec04c72938f49a5fc1246744
nikita-vanyasin <nikita.vanyasin@gmail.com>
2018-02-16 00:28:41 +0300

    -added dunst config. fixed battery checking.

--- ansible/roles/i3wm/templates/check-battery-level.sh
+++ ansible/roles/i3wm/templates/check-battery-level.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 BATTINFO=`acpi -b`
-if [[ `echo $BATTINFO | grep Discharging` && `echo $BATTINFO | cut -f 5 -d " "` < 00:15:00 ]] ; then
-    DISPLAY=:0.0 zenity --error --text "Low battery! $BATTINFO"
+if [[ `echo $BATTINFO | grep Discharging` && `echo $BATTINFO | cut -f 5 -d " "` < 10:15:00 ]] ; then
+    aplay -q ~/.config/i3/alarm_beep.wav &
+    zenity --display=:2 --error --text "Low battery! $BATTINFO"
 fi
