10f63ad14ba517c1b9638aeed21f9c3bb7a917af
nikita-vanyasin <nikita.vanyasin@gmail.com>
2018-03-22 15:02:00 +0300

    fixed battery level check

--- check-battery-level.sh
+++ check-battery-level.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 BATTINFO=`acpi -b`
-if [[ `echo $BATTINFO | grep Discharging` && `echo $BATTINFO | cut -f 5 -d " "` < 10:15:00 ]] ; then
-    zenity --display=:2 --error --text "Low battery! $BATTINFO"
+if [[ `echo $BATTINFO | grep Discharging` && `echo $BATTINFO | cut -f 5 -d " "` < 00:15:00 ]] ; then
+    zenity --display=$DISPLAY --error --text "Low battery! $BATTINFO"
 fi
