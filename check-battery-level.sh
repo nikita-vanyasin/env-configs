13612886328a9deb4418e44ecdf5e344ac172a34
nikita-vanyasin <nikita.vanyasin@gmail.com>
2018-04-29 17:09:59 +0300

    - added vpn status, removed vim, etc

--- check-battery-level.sh
+++ check-battery-level.sh
@@ -2,5 +2,5 @@
 
 BATTINFO=`acpi -b`
 if [[ `echo $BATTINFO | grep Discharging` && `echo $BATTINFO | cut -f 5 -d " "` < 00:15:00 ]] ; then
-    zenity --display=$DISPLAY --error --text "Low battery! $BATTINFO"
+    zenity --error --text "Low battery! $BATTINFO"
 fi
