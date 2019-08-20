2cd5935c8bd11dfd46ce2c790ec329bc670a71ab
nikita-vanyasin <nikita.vanyasin@gmail.com>
2019-08-20 11:21:44 +0300

    add grive, eye-rest and i3-battery-popup

--- check-battery-level.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-BATTINFO=`acpi -b`
-if [[ `echo $BATTINFO | grep Discharging` && `echo $BATTINFO | cut -f 5 -d " "` < 00:15:00 ]] ; then
-    zenity --error --text "Low battery! $BATTINFO"
-fi
---
+++ eye-rest.sh
@@ -0,0 +1,3 @@
+
+
+zenity --error --text "Time to rest your eyes for 20 seconds!"
